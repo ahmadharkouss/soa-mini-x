d303a8a590c56d783a089ada97d82038ce5a0d19
ahmadharkouss <71528971+ahmadharkouss@users.noreply.github.com>
2024-07-03 14:34:31 +0200

    solved bug pg

--- keycloak_backup/restore.sh
+++ keycloak_backup/restore.sh
@@ -9,10 +9,7 @@ until pg_isready -U keycloak; do
   sleep 1
 done
 
-# Check if the database exists, create if it doesn't
-psql -U keycloak -tc "SELECT 1 FROM pg_database WHERE datname = 'keycloak'" | grep -q 1 || psql -U keycloak -c "CREATE DATABASE keycloak"
-
-# Restore the backup if it exists
+# Restore the backup
 if [ -f /docker-entrypoint-initdb.d/keycloak_backup.sql ]; then
   psql -U keycloak -d keycloak < /docker-entrypoint-initdb.d/keycloak_backup.sql
 fi
